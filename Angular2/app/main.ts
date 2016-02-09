362ea6ac89b6bfb7a8bbc2a6caed473158282a32
Krzysztof Lis <klis@outlook.com>
2016-02-10 00:06:37 +0100

    Introduced Heroes dashboard

--- Angular2/app/main.ts
+++ Angular2/app/main.ts
@@ -2,7 +2,7 @@
  * Created by klis on 08/02/16.
  */
 
-import {bootstrap} from 'angular2/platform/browser'
-import {AppComponent} from './app.component'
+import {bootstrap} from 'angular2/platform/browser';
+import {AppComponent} from './app.component';
 
 bootstrap(AppComponent);
